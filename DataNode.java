1d9046a5c0281d3aa02369fd17cbe19512224843
byh11 <97822382+byh11@users.noreply.github.com>
2022-01-18 11:03:31 +0800

    DataNode

---
+++ DataNode.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class DataNode {
+    private String id;
+    private String key;
+    private String val;
+    private long time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
